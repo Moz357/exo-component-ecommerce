9932e386688ed42ff32660e4d2cdb496d923b0eb
Mohamed Bougrinat <mohamed.bougrinat@lapiscine.pro>
2023-11-08 11:43:20 +0100

    rajout userlogged

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,39 @@
-function Header (){
-    const isUserLogged = false;
+function Header() {
+    const userLogged = {
+        lastname : "BOUGRINAT",
+        firstname : "Mohamed",
+        job : "Web Dev",
+
+    }
+    
+    const isUserLogged = true;
+    const itemsInCart = 10;
+  
     return (
-        <header>
-        <h1>Super U ecommerce</h1>
+      <header>
+        <h1>Super ecommerce</h1>
         <nav>
           <ul>
+          {isUserLogged ? (
+            <li>
+              {userLogged.firstname} {userLogged.lastname} - {userLogged.job}
+            </li>
+          ) : (
+            <li>Connectez-vous</li>
+          )}
+          
+          
+  
             <li>Home</li>
             <li>Products</li>
-
-            {isUserLogged ? <li>Mohamed Bougrinat</li> : <li>Connectez-vous</li>}
-
+  
+            {itemsInCart > 0 ? <li>Cart : {itemsInCart}</li> : <li>Pas d'item au panier</li>}
           </ul>
         </nav>
       </header>
-
     );
+  }
+  
+  export default Header;
 
-}
 
-export default (Header);
